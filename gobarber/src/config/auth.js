3c10eafb25d55edc8a4b8ea26f41e5f6640e46eb
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-06-26 00:01:42 -0300

    Finalizado módulo 1

---
+++ gobarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'token_maroto',
+  expiresIn: '7d',
+};
--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello' }));
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
