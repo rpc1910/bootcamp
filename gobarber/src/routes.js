854c2398e0172a811cd9e72a29d0270c010d57a3
Rodrigo Passos <rodrigo.cunha@etapa.com.br>
2019-07-10 18:07:47 -0300

    Salvando arquivos no banco de dados e criando relacionamento para a tabela users

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -5,6 +5,7 @@ import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,8 +16,6 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json(['OK']);
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
