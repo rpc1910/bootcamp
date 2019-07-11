fa30ac2b448637e74ee8fa297961e2e29841fd1a
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-07-10 23:38:56 -0300

    feat: Add Appointment controller

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,5 +20,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
