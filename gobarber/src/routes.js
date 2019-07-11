c0fe700b0b834874be3237510395fce5c05479cf
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-07-10 23:08:32 -0300

    Add ProviderController and setup assets url

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -6,6 +6,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,7 +16,8 @@ routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
