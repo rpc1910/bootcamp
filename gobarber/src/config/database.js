0924a57ed98dc936118588b9ca73531342de9f12
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-07-11 23:10:36 -0300

    feat: Finished mongodb integration

--- gobarber/src/config/database.js
+++ gobarber/src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   dialect: 'postgres',
-  // host: '192.168.99.100',
-  host: 'localhost',
+  host: '192.168.99.100',
+  // host: 'localhost',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -25,7 +25,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/gobarber',
+      'mongodb://192.168.99.100:27017/gobarber',
       {
         useNewUrlParser: true,
         useFindAndModify: true,
