c8157a1d3790690175133279399dbfa63e90c4af
Rodrigo Passos <rodrigo.cunha@etapa.com.br>
2019-07-16 15:22:15 -0300

    feat: Add Sentry integration and dotenv

--- gobarber/src/config/auth.js
+++ gobarber/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'token_maroto',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- gobarber/src/config/mail.js
+++ gobarber/src/config/mail.js
@@ -1,12 +1,12 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  secure: false,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE,
   auth: {
-    user: '790cdc3ed3235b',
-    pass: '7c07715ef24418',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
-  proxy: '',
+  proxy: process.env.MAIL_PROXY,
   default: {
     from: 'Equipe Gobarber <noreply@rodrigopassos.com.br>',
   },
